a793004564f35f416ae0580c0b90c0c670cf2206
Sunil Sankar <nagioskeeper@gmail.com>
2025-06-20 13:14:25 +0200

    Added the vm configs

--- terraform/main/vms.tf
+++ terraform/main/vms.tf
@@ -1,5 +1,5 @@
 module "vm1"{
-    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.0"
+    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.1"
     name = "vm1"
     description = "Used for testing"
     vmid = 100
@@ -11,7 +11,7 @@ module "vm1"{
     PUBLIC_SSH_KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDON13U0/qvu9XcxGYEadjR2oWIgYjVCXycCpz/qrI1Y sunil@Sunils-Mac-mini.local"
 }
 module "vm2"{
-    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.0"
+    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.1"
     name = "vm2"
     description = "Used for testing2"
     vmid = 101
@@ -23,14 +23,14 @@ module "vm2"{
     PUBLIC_SSH_KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDON13U0/qvu9XcxGYEadjR2oWIgYjVCXycCpz/qrI1Y sunil@Sunils-Mac-mini.local"
 }
 module "vm3"{
-    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.0"
+    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.1"
     name = "vm3"
     description = "Used for testing3"
     vmid = 102
-    templatename = "debian12-Template"
+    templatename = "ubuntu24.04-Template"
     cores = 1
     memory = 512
-    disksize = "10G"
+    disksize = "5G"
     ipaddress = "192.168.1.44"
     PUBLIC_SSH_KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDON13U0/qvu9XcxGYEadjR2oWIgYjVCXycCpz/qrI1Y sunil@Sunils-Mac-mini.local"
 }
