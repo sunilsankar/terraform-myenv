f1a231fb3f30fdd24366e97aa99b99f49e874ff3
Sunil Sankar <nagioskeeper@gmail.com>
2025-06-20 13:37:27 +0200

    Update vms.tf

--- terraform/main/vms.tf
+++ terraform/main/vms.tf
@@ -1,36 +1,14 @@
-module "vm1"{
+module "vms"{
+    for_each = local.vms
     source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.1"
-    name = "vm1"
-    description = "Used for testing"
-    vmid = 100
-    templatename = "alma9-Template"
-    cores = 1
-    memory = 512
-    disksize = "12G"
-    ipaddress = "192.168.1.42"
-    PUBLIC_SSH_KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDON13U0/qvu9XcxGYEadjR2oWIgYjVCXycCpz/qrI1Y sunil@Sunils-Mac-mini.local"
+    name = each.value["name"]
+    description = each.value["description"]
+    vmid = each.value["vmid"]
+    templatename = each.value["templatename"]
+    cores = each.value["cores"]
+    memory = each.value["memory"]
+    disksize = each.value["disksize"]
+    ipaddress = each.value["ipaddress"]
+    PUBLIC_SSH_KEY = each.value["PUBLIC_SSH_KEY"]
 }
-module "vm2"{
-    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.1"
-    name = "vm2"
-    description = "Used for testing2"
-    vmid = 101
-    templatename = "alma10-Template"
-    cores = 1
-    memory = 512
-    disksize = "10G"
-    ipaddress = "192.168.1.43"
-    PUBLIC_SSH_KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDON13U0/qvu9XcxGYEadjR2oWIgYjVCXycCpz/qrI1Y sunil@Sunils-Mac-mini.local"
-}
-module "vm3"{
-    source = "git::https://github.com/sunilsankar/tf-module-proxmox.git?ref=0.1.1"
-    name = "vm3"
-    description = "Used for testing3"
-    vmid = 102
-    templatename = "ubuntu24.04-Template"
-    cores = 1
-    memory = 512
-    disksize = "5G"
-    ipaddress = "192.168.1.44"
-    PUBLIC_SSH_KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDON13U0/qvu9XcxGYEadjR2oWIgYjVCXycCpz/qrI1Y sunil@Sunils-Mac-mini.local"
-}
+
